3fa54b77c530442132011e77463ef947e5a0c4ae
bonniesdad <bonniesnerdydad@gmail.com>
2026-06-24 12:21:46 +0100

    login send

--- Functions/Sunglitters/SendDeathTaxLeaderboardSync.lua
+++ Functions/Sunglitters/SendDeathTaxLeaderboardSync.lua
@@ -22,32 +22,69 @@ function FreshSoD_SendDeathTaxLeaderboardSync(playerName, totalCopper)
   return true
 end
 
+local loginLeaderboardSyncSent = false
+
 function FreshSoD_BroadcastDeathTaxLeaderboardSync()
+  if not IsInGuild() then
+    FreshSoD_LogDeathTax('Leaderboard broadcast skipped: not in guild')
+    return false
+  end
+
   if not FreshSoD_IsDeathTaxGuild() then
-    return
+    FreshSoD_LogDeathTax('Leaderboard broadcast skipped: not a death tax guild (guild=' .. tostring(FreshSoD_GetPlayerGuildName and FreshSoD_GetPlayerGuildName()) .. ')')
+    return false
   end
 
   FreshSoD_SyncLocalDeathTaxLeaderboardEntry()
 
   local playerName = UnitName('player')
   if not playerName then
+    FreshSoD_LogDeathTax('Leaderboard broadcast skipped: no player name')
+    return false
+  end
+
+  local totalCopper = FreshSoD_GetDeathTaxTotalAccumulatedCopper()
+  local owedCopper = FreshSoD_GetDeathTaxOwedCopper()
+  FreshSoD_LogDeathTax(
+    'Leaderboard broadcast: owed=' .. tostring(owedCopper) .. 'c total=' .. tostring(totalCopper) .. 'c'
+  )
+
+  return FreshSoD_SendDeathTaxLeaderboardSync(playerName, totalCopper)
+end
+
+local function tryLoginLeaderboardSync()
+  if loginLeaderboardSyncSent then
     return
   end
 
-  FreshSoD_SendDeathTaxLeaderboardSync(playerName, FreshSoD_GetDeathTaxTotalAccumulatedCopper())
+  if FreshSoD_BroadcastDeathTaxLeaderboardSync() then
+    loginLeaderboardSyncSent = true
+    FreshSoD_LogDeathTax('Login leaderboard sync complete')
+  end
+end
+
+local function scheduleLoginLeaderboardSync(delay)
+  if C_Timer and C_Timer.After then
+    C_Timer.After(delay, tryLoginLeaderboardSync)
+  else
+    tryLoginLeaderboardSync()
+  end
 end
 
 local leaderboardSyncFrame = CreateFrame('Frame')
 leaderboardSyncFrame:RegisterEvent('PLAYER_LOGIN')
+leaderboardSyncFrame:RegisterEvent('PLAYER_GUILD_UPDATE')
+leaderboardSyncFrame:RegisterEvent('GUILD_ROSTER_UPDATE')
 
-leaderboardSyncFrame:SetScript('OnEvent', function()
-  if not FreshSoD_IsDeathTaxGuild() then
+leaderboardSyncFrame:SetScript('OnEvent', function(_, event)
+  if event == 'PLAYER_LOGIN' then
+    loginLeaderboardSyncSent = false
+    FreshSoD_LogDeathTax('Scheduling login leaderboard sync')
+    scheduleLoginLeaderboardSync(3)
     return
   end
 
-  if C_Timer and C_Timer.After then
-    C_Timer.After(3, FreshSoD_BroadcastDeathTaxLeaderboardSync)
-  else
-    FreshSoD_BroadcastDeathTaxLeaderboardSync()
+  if not loginLeaderboardSyncSent then
+    tryLoginLeaderboardSync()
   end
 end)
